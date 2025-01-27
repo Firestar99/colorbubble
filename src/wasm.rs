c15cffb9199f511d35186d647b8ababac61d8f9b
Firestar99 <firestar99@sydow.cloud>
2025-01-27 17:06:44 +0100

    wasm compiles but panics

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,7 +1,7 @@
 #![cfg(target_arch = "wasm32")]
 
 use crate::main_loop::run;
-use wasm_bindgen::JsCast;
+use wasm_bindgen::prelude::*;
 use winit::event_loop::EventLoop;
 use winit::platform::web::WindowBuilderExtWebSys;
 
@@ -9,20 +9,20 @@ use winit::platform::web::WindowBuilderExtWebSys;
 
 #[wasm_bindgen(start)]
 pub fn wasm_start() {
-    wasm_run().unwrap()
-}
-
-pub fn wasm_run() -> anyhow::Result<()> {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-    let event_loop = EventLoop::new()?;
-    let canvas = web_sys::window()?
-        .document()?
-        .get_element_by_id("canvas")?
-        .dyn_into::<web_sys::HtmlCanvasElement>()?;
+    let event_loop = EventLoop::new().unwrap();
+    let canvas = web_sys::window()
+        .unwrap()
+        .document()
+        .unwrap()
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
     let window = winit::window::WindowBuilder::new()
         .with_canvas(Some(canvas))
-        .build(&event_loop)?;
-    console_log::init()?;
-    wasm_bindgen_futures::spawn_local(run(event_loop, window));
-    Ok(())
+        .build(&event_loop)
+        .unwrap();
+    console_log::init().unwrap();
+    wasm_bindgen_futures::spawn_local(async { run(event_loop, window).await.unwrap() });
 }
