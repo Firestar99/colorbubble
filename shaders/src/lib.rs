f7ffb025956a25997a7cf33d26ed2491bcb586cf
Firestar99 <firestar99@sydow.cloud>
2025-05-28 13:23:23 +0200

    fixed naga failing to transpile shaders

--- shaders/src/lib.rs
+++ shaders/src/lib.rs
@@ -1,13 +1,9 @@
 #![no_std]
 
 use glam::{Vec2, Vec4, Vec4Swizzles};
-use spirv_std::image::Image2d;
+use spirv_std::image::sample_with::lod;
+use spirv_std::image::{Image2d, ImageWithMethods};
 use spirv_std::{Sampler, spirv};
-
-pub struct VertexOutput {
-    tex_coord: Vec2,
-    vtx_color: Vec4,
-}
 
 pub struct FrameData {
     viewport: Vec4,
@@ -18,12 +14,13 @@ pub fn vs_main(
     #[spirv(descriptor_set = 0, binding = 0, uniform)] frame_data: &FrameData,
     position: Vec2,
     tex_coord: Vec2,
-    vtx_color: Vec4,
-    result: &mut VertexOutput,
+    color: Vec4,
+    vtx_color: &mut Vec4,
+    vtx_tex_coord: &mut Vec2,
     #[spirv(position)] gl_position: &mut Vec4,
 ) {
-    result.tex_coord = tex_coord;
-    result.vtx_color = vtx_color;
+    *vtx_tex_coord = tex_coord;
+    *vtx_color = color;
     *gl_position = Vec4::from((
         position * frame_data.viewport.zw() + frame_data.viewport.xy(),
         0.,
@@ -32,18 +29,19 @@ pub fn vs_main(
 }
 
 #[spirv(fragment)]
-pub fn fs_color(vertex: VertexOutput, color_out: &mut Vec4) {
-    *color_out = vertex.vtx_color;
+pub fn fs_color(vtx_color: Vec4, _vtx_tex_coord: Vec2, color_out: &mut Vec4) {
+    *color_out = Vec4::from(vtx_color);
 }
 
 #[spirv(fragment)]
 pub fn fs_texture(
-    vertex: VertexOutput,
+    vtx_color: Vec4,
+    vtx_tex_coord: Vec2,
     #[spirv(descriptor_set = 1, binding = 0)] my_texture: &Image2d,
     #[spirv(descriptor_set = 1, binding = 1)] my_sampler: &Sampler,
     color_out: &mut Vec4,
 ) {
-    *color_out = my_texture.sample(*my_sampler, vertex.tex_coord) * vertex.vtx_color;
+    *color_out = my_texture.sample(*my_sampler, vtx_tex_coord) * vtx_color;
     if color_out.w < 0.01 {
         spirv_std::arch::kill();
     }
@@ -51,15 +49,18 @@ pub fn fs_texture(
 
 #[spirv(fragment)]
 pub fn fs_masked(
-    vertex: VertexOutput,
+    vtx_color: Vec4,
+    vtx_tex_coord: Vec2,
     #[spirv(frag_coord)] frag_coord: Vec4,
     #[spirv(descriptor_set = 1, binding = 0)] my_texture: &Image2d,
     #[spirv(descriptor_set = 1, binding = 1)] my_sampler: &Sampler,
     #[spirv(descriptor_set = 2, binding = 0)] mask_texture: &Image2d,
     color_out: &mut Vec4,
 ) {
-    *color_out = my_texture.sample(*my_sampler, vertex.tex_coord) * vertex.vtx_color;
-    let mask = mask_texture.fetch(frag_coord.xy().as_uvec2()).x;
+    *color_out = my_texture.sample(*my_sampler, vtx_tex_coord) * vtx_color;
+    let mask = mask_texture
+        .fetch_with(frag_coord.xy().as_uvec2(), lod(0))
+        .x;
     if mask < 0.01 || color_out.w < 0.01 {
         spirv_std::arch::kill();
     }
